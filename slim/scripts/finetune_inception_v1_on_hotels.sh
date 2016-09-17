3a2cd8be79445f7edc117a1c1019af49f71176f8
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2016-09-17 17:18:57 +0700

    Only do training all layers

--- slim/scripts/finetune_inception_v1_on_hotels.sh
+++ slim/scripts/finetune_inception_v1_on_hotels.sh
@@ -37,45 +37,45 @@ echo
 echo '======================================================================='
 echo -ne '\n\n\n\n\n\n\n\n'
 
-echo '------------------------ TRAIN THE LAST LAYER -------------------------'
-# Fine-tune only the new layers.
-python "$THIS_DIR/../train_image_classifier.py" \
-  --train_dir=${TRAIN_DIR} \
-  --dataset_name=hotels \
-  --dataset_split_name=train \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=inception_v1 \
-  --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v1.ckpt \
-  --checkpoint_exclude_scopes=InceptionV1/Logits \
-  --trainable_scopes=InceptionV1/Logits \
-  --max_number_of_steps=300000 \
-  --batch_size=32 \
-  --learning_rate=0.01 \
-  --save_interval_secs=3600 \
-  --save_summaries_secs=120 \
-  --log_every_n_steps=100 \
-  --optimizer=rmsprop \
-  --weight_decay=0.00004 \
-  --num_clones=4 \
-  --num_readers=8 \
-  --num_preprocessing_threads=32
-echo
-echo '======================================================================='
-echo -ne '\n\n\n\n\n\n\n\n'
+# echo '------------------------ TRAIN THE LAST LAYER -------------------------'
+# # Fine-tune only the new layers.
+# python "$THIS_DIR/../train_image_classifier.py" \
+#   --train_dir=${TRAIN_DIR} \
+#   --dataset_name=hotels \
+#   --dataset_split_name=train \
+#   --dataset_dir=${DATASET_DIR} \
+#   --model_name=inception_v1 \
+#   --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v1.ckpt \
+#   --checkpoint_exclude_scopes=InceptionV1/Logits \
+#   --trainable_scopes=InceptionV1/Logits \
+#   --max_number_of_steps=300000 \
+#   --batch_size=32 \
+#   --learning_rate=0.01 \
+#   --save_interval_secs=3600 \
+#   --save_summaries_secs=120 \
+#   --log_every_n_steps=100 \
+#   --optimizer=rmsprop \
+#   --weight_decay=0.00004 \
+#   --num_clones=4 \
+#   --num_readers=8 \
+#   --num_preprocessing_threads=32
+# echo
+# echo '======================================================================='
+# echo -ne '\n\n\n\n\n\n\n\n'
 
-echo '----------------- EVALUATE TRAINING OF THE LAST LAYER -----------------'
-# Run evaluation.
-python "$THIS_DIR/../eval_image_classifier.py" \
-  --checkpoint_path=${TRAIN_DIR} \
-  --eval_dir=${TRAIN_DIR} \
-  --dataset_name=hotels \
-  --dataset_split_name=validation \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=inception_v1 \
-  --num_preprocessing_threads=32
-echo
-echo '======================================================================='
-echo -ne '\n\n\n\n\n\n\n\n'
+# echo '----------------- EVALUATE TRAINING OF THE LAST LAYER -----------------'
+# # Run evaluation.
+# python "$THIS_DIR/../eval_image_classifier.py" \
+#   --checkpoint_path=${TRAIN_DIR} \
+#   --eval_dir=${TRAIN_DIR} \
+#   --dataset_name=hotels \
+#   --dataset_split_name=validation \
+#   --dataset_dir=${DATASET_DIR} \
+#   --model_name=inception_v1 \
+#   --num_preprocessing_threads=32
+# echo
+# echo '======================================================================='
+# echo -ne '\n\n\n\n\n\n\n\n'
 
 echo '-------------------------- TRAIN ALL LAYERS ---------------------------'
 # Fine-tune all the new layers.
