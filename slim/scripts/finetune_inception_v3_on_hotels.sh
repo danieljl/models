ccf9fc905fa6538127f9365d65677a033bac8f56
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2016-09-23 22:14:26 +0700

    Revert learning rate to the original

--- slim/scripts/finetune_inception_v3_on_hotels.sh
+++ slim/scripts/finetune_inception_v3_on_hotels.sh
@@ -90,10 +90,7 @@ python "$THIS_DIR/../train_image_classifier.py" \
   --model_name=inception_v3 \
   --max_number_of_steps=200000 \
   --batch_size=16 \
-  --learning_rate=0.0002 \
-  --learning_rate_decay_type=exponential \
-  --learning_rate_decay_factor=0.75 \
-  --num_epochs_per_decay=0.1358 \
+  --learning_rate=0.0001 \
   --save_interval_secs=600 \
   --save_summaries_secs=120 \
   --log_every_n_steps=100 \
